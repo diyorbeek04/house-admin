5ad86a90f406c1a6e24552b51ef89d33e97f6267
Shekhri <khamidovashakhrezada3868@gmail.com>
2024-05-31 14:16:59 +0500

    partners

--- src/components/contact/index.jsx
+++ src/components/contact/index.jsx
@@ -1,13 +1,83 @@
-import React from "react";
-import styles from './style.module.css'
+import React, { useRef, useState, useEffect } from 'react';
+import axios from 'axios';
+import Styles from './style.module.css';
+import ClearIcon from '@mui/icons-material/Clear';
 
-
-function ContactComponent() {
+const ContactComponent = () => {
+  const [tasks, contact] = useState([]);
+  const [taskToEdit, setTaskToEdit] = useState();
+  const [open, setOpen] = useState(false);
+  const imgRef = useRef();
+  
+function MoreNFTComponent() {
    return(
       <>
-         Contact
+      Hello
       </>
    )
 }
 
-export default ContactComponent
+  useEffect(() => {
+    axios.get('http://localhost:8000/contact')
+      .then(res => contact(res.data))
+      .catch(err => console.error('Error fetching data:', err));
+  }, []);
+
+
+  const handleEdit = (task) => {
+    setTaskToEdit(task);
+    setOpen(true);
+  };
+
+  const handleSubmitEdit = () => {
+    if (imgRef.current.value ) {
+      const body = {
+         contact_email: imgRef.current.value
+      };
+      axios.put(`http://localhost:8000/contact/${taskToEdit.id}`, body)
+        .then(res => {
+          const updatedTasks = tasks.map(item => (item.id === taskToEdit.id ? res.data : item));
+          contact(updatedTasks);
+          setOpen(false);
+        })
+
+    } else {
+      alert(`Please fill in all fields`);
+    }
+  };
+
+  const handleDelete = (id) => {
+    axios.delete(`http://localhost:8000/contact/${id}`)
+      .then(() => {
+        const updatedTasks = tasks.filter(item => item.id !== id);
+        contact(updatedTasks);
+        setOpen(false);
+      })
+  };
+
+  return (
+    <>
+      <div className={Styles.section}>
+        <div className={Styles.container}>
+          
+          {tasks.map((task, index) => (
+            <div key={index} className={Styles.card}>
+              <p className={Styles.img} >{task.contact_email}</p>
+              
+            </div>
+          ))}
+        </div>
+        {open && (
+          <>
+           
+          </>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default ContactComponent;
+
+
+// export default ContactComponent
--- src/components/partners/index.jsx
+++ src/components/partners/index.jsx
@@ -1,13 +1,97 @@
-import React from "react";
-import styles from './style.module.css'
+import React, { useRef, useState, useEffect } from 'react';
+import axios from 'axios';
+import Styles from './style.module.css';
+import ClearIcon from '@mui/icons-material/Clear';
 
+const PartnersComponent = () => {
+   const [tasks, Partners] = useState([]);
+   const [taskToEdit, setTaskToEdit] = useState(null);
+   const [open, setOpen] = useState(false);
+   const imgRef = useRef();
+   const titleRef = useRef();
+   const descRef = useRef();
+   const priceRef = useRef();
+   const salesRef = useRef();
 
-function PartnersComponent() {
-   return(
+   useEffect(() => {
+      axios.get('http://localhost:8000/Partners')
+         .then(res => Partners(res.data))
+         .catch(err => console.error('Error fetching data:', err));
+   }, []);
+
+   const handleEdit = (task) => {
+      setTaskToEdit(task);
+      setOpen(true);
+   };
+
+   const handleSubmitEdit = () => {
+      if (imgRef.current.value && titleRef.current.value && descRef.current.value && priceRef.current.value && salesRef.current.value && taskToEdit) {
+         const body = {
+            Partners_logo: imgRef.current.value,
+         
+         };
+         axios.put(`http://localhost:8000/Partners/${taskToEdit.id}`, body)
+            .then(res => {
+               const updatedTasks = tasks.map(item => (item.id === taskToEdit.id ? res.data : item));
+               Partners(updatedTasks);
+               setOpen(false);
+            })
+
+      } else {
+         alert(`Please fill in all fields`);
+      }
+   };
+
+   const handleDelete = (id) => {
+      axios.delete(`http://localhost:8000/Partners/${id}`)
+         .then(() => {
+            const updatedTasks = tasks.filter(item => item.id !== id);
+            Partners(updatedTasks);
+            setOpen(false);
+         })
+   };
+
+   return (
       <>
-         Partners
-      </>
-   )
-}
+         <div className={Styles.section}>
+            <div className={Styles.container}>
+            <div className={Styles.card}>
+            <form className={Styles.form}>
+              <div className={Styles.card__modal}>
+                <input className={Styles.input__top} ref={imgRef} type='url' placeholder='Rasm' />
+              </div>
+              <button className={Styles.card__modal__button}>Qo'shish</button>
+            </form>
+          </div>
 
-export default PartnersComponent
+               {tasks.map((task, index) => (
+                  <div key={index} className={Styles.card}>
+                     <img className={Styles.img} src={task.Partners_logo} alt="" />
+                     <div className={Styles.card__right}>
+                        {/* <button className={Styles.card__button}>Post qo'shish</button> */}
+                        <button className={Styles.card__button} onClick={() => handleEdit(task)}>Edit</button>
+                        <button className={Styles.card__button} onClick={() => handleDelete(task.id)}>Delete</button>
+                     </div>
+                  </div>
+               ))}
+            </div>
+            {open && (
+               <>
+                  <div className={Styles.modal}>
+                     <div className={Styles.modal__item}>
+                        <ClearIcon className={Styles.icon} onClick={() => setOpen(false)} />
+                     </div>
+                     <form>
+                        <input className={Styles.input} ref={imgRef} type='url' required defaultValue={taskToEdit.Partners_logo} placeholder='Rasm' />
+                        <button className={Styles.modal__button} onClick={handleSubmitEdit}>Saqlash</button>
+                     </form>
+                  </div>
+                  <div className={Styles.overlay} onClick={() => setOpen(false)} />
+               </>
+            )}
+         </div>
+      </>
+   );
+};
+
+export default PartnersComponent;
